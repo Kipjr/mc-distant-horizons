5577261774535be7dfde98cb4709951a9d418823
s809 <11816467-s809@users.noreply.gitlab.com>
2023-12-18 23:11:47 +0500

    Update wipe script Update core

--- wipelod.sh
+++ wipelod.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-rm -rf fabric/run/client/Distant_Horizons_server_data
-rm -rf $(find fabric/run/server -name "Distant_Horizons")
-
-# TODO Forge folders
+rm -rf run/client/Distant_Horizons_server_data
+rm -rf $(find run/server -name "Distant_Horizons")
+rm -rf $(find run/server -name "DistantHorizons.sqlite")
