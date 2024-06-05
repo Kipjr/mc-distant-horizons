fb25423deb11c6b8b68e209b8fdd112162370ad5
s809 <11816467-s809@users.noreply.gitlab.com>
2024-06-05 21:17:21 +0500

    Fix compilation for older versions 2

--- forge/src/main/java/com/seibel/distanthorizons/forge/mixins/client/MixinClientPacketListener.java
+++ forge/src/main/java/com/seibel/distanthorizons/forge/mixins/client/MixinClientPacketListener.java
@@ -2,10 +2,8 @@ package com.seibel.distanthorizons.forge.mixins.client;
 
 import com.seibel.distanthorizons.common.wrappers.world.ClientLevelWrapper;
 import com.seibel.distanthorizons.core.api.internal.ClientApi;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,11 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPacketListener.class)
 public class MixinClientPacketListener
 {
+	// TODO update fabric version as well
+	
 	@Inject(method = "handleLogin", at = @At("RETURN"))
-	void onHandleLoginEnd(CallbackInfo ci)
-	{
-		ClientApi.INSTANCE.onClientOnlyConnected();
-	}
+	void onHandleLoginEnd(CallbackInfo ci) { ClientApi.INSTANCE.onClientOnlyConnected(); }
 	
 	#if MC_VER < MC_1_19_4
 	@Inject(method = "cleanup", at = @At("HEAD"))
